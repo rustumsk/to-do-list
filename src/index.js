1687b3abbd9cfd5dc78a006fda1dcdeb4d83702c
russ <136210947+rustumsk@users.noreply.github.com>
2024-03-15 06:13:48 +0000

    slight design and animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.scss'
+
+const cB = document.querySelector(".open");
+cB.addEventListener("click", () =>{
+    const nB = document.querySelector(".nav-bar");
+    
+    nB.classList.toggle("close");
+
+})
